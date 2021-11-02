d6103016c3efbfe8a265801ad6a11e6c2d076080
angelfacundotorres <torresangelfacundo@gmail.com>
2021-11-01 22:52:57 -0300

    primerCommit

---
+++ js/tp1.js
@@ -0,0 +1,98 @@
+// 1)
+
+// alert("Un Mensaje")
+
+// 2)
+
+// document.write('Hello World')
+
+// 3)
+
+// document.write(3+5)
+  
+//  4)
+
+// var userName = prompt('Por favor ingresa tu nombre');
+// document.write('Hola ' +userName+ ' bienvenido');
+
+// 5)
+
+// var number = parseInt(prompt('Por favor ingresa un númer'));
+// var number2 = parseInt(prompt('Por favor ingresa otro númer'));
+// document.write(number + number2);
+
+// 6)
+
+// var number = parseInt(prompt('Por favor ingresa un númer'));
+// var number2 = parseInt(prompt('Por favor ingresa otro númer'));
+// if (number > number2) {
+//   document.write("El mayor número es: " +  number);
+// } else {
+//   document.write("El mayor número es: " +  number2);
+// }
+
+// 7)
+
+// var number = parseInt(prompt('Por favor ingresa un númer'));
+// var number2 = parseInt(prompt('Por favor ingresa otro númer'));
+// var number3 = parseInt(prompt('Por favor ingresa un último número númer'));
+// if (number > number2 && number > number3) {
+//   document.write(number)
+// } else if (number2 > number && number2 > number3){
+//   document.write(number2)
+// } else {
+//   document.write(number3)
+// }
+
+
+// 8)
+
+// var dividendo = parseInt(prompt("Ingresa un numero"))
+// if (dividendo % 2 == 0) {
+//   document.write("El número ingresado es divisible de 2")
+// } else {
+//   document.write("El numero ingresado no es divisible de 2")
+// }
+
+//   9)
+
+// let frase = prompt('Ingresa una frase')
+
+// console.log(frase.length)
+// console.log(frase.toLowerCase())
+// frase = frase.toLowerCase()
+
+// for (let i = 0; i < frase.length; i++) {
+// if (frase.charAt(i) == 'a' || frase.charAt(i) == 'á' || frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'é' || frase.charAt(i) == 'i' || frase.charAt(i) == 'í' || frase.charAt(i) == 'o' || frase.charAt(i) == 'ó' || frase.charAt(i) == 'u' || frase.charAt(i) == 'ú') {
+//   document.write(frase.charAt(i))  
+// }  
+// }
+
+//   10)
+
+// let dividendo = parseInt(prompt('Por favor ingresa un número'))
+// if (dividendo % 2 == 0 || dividendo % 3 == 0 || dividendo % 5 == 0 || dividendo % 7 == 0) {
+//   document.write('El númeroingresado si es divisible por uno de los números asignados')
+// } else {
+//   document.write('El numero ingresado no es divisible')
+// }
+
+//   11)
+// let number = parseInt(prompt('Ingresa un número'))
+
+// if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0) {
+//   if (number % 2 == 0) {
+//     document.write('Es divisible por 2')
+//   }
+//   if (number % 3 == 0) {
+//     document.write('Es divisible por 3')
+//   }
+//   if (number % 5 == 0) {
+//     document.write('Es divisible por 5')
+//   }
+//   if (number % 7 == 0) {
+//       document.write('Es divisible por 7')
+//   }  
+// } else {
+//   document.write('No es divisible por ninguno de los posibles divisores')
+// }
